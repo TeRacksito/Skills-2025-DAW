a0da461e525db7cb6ac24fc7f6b0689c5d58af3d
TeRacksito <acho.chuchi.8@gmail.com>
2025-03-13 09:33:48 +0000

    changes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,15 +1,28 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ClientsController;
 use App\Http\Controllers\WelcomePhraseController;
+use App\Models\Administrators;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/health', function () {
+Route::get('health', function () {
     return response()->json(['status' => 'ok']);
 });
 
-Route::get('/welcome-phrase', [WelcomePhraseController::class, 'index']);
+Route::apiResource("clients", ClientsController::class);
+Route::apiResource("administrators", ClientsController::class);
+Route::apiResource("businesses", ClientsController::class);
+Route::apiResource("categories", CategoriesController::class);
+Route::apiResource("products", ClientsController::class);
+Route::apiResource("services", ClientsController::class);
+Route::apiResource("events", ClientsController::class);
+Route::apiResource("businessmen", ClientsController::class);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
